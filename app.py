8d6496bba6dd97c72b43426cdfe0ae81f18f59fb
masakingko <139669364+masakingko@users.noreply.github.com>
2023-11-15 12:01:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.Chat.Completions.create(
+    response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages,
         temperature=y,
