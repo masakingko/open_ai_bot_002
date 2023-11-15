3bc620e0b7b65796892765183cd22c49f634b6e1
masakingko <139669364+masakingko@users.noreply.github.com>
2023-11-15 11:58:37 +0900

    Update app.py
    chat.completionsに変更

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
+    response = openai.Chat.Completions.create(
         model="gpt-3.5-turbo",
         messages=messages,
         temperature=y,
